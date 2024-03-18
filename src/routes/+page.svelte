ef5672daef74965f5297250928727feedb7f0b96
leobewater <mmbee888@gmail.com>
2024-03-18 14:34:21 -0400

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-  import Hero from "../components/Hero.svelte";
-  import Product from "../components/Product.svelte";
+  // import Hero from "../components/Hero.svelte";
+  // import Product from "../components/Product.svelte";
 </script>
 
 <main class="flex flex-col">
-  <Hero />
-  <Product />
+  <!-- <Hero /> -->
+  <!-- <Product /> -->
+  <h1>Welcome Home!</h1>
 </main>
