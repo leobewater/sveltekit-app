a14aa040093e710fdeb672d49858b1be2abb3b4d
leobewater <mmbee888@gmail.com>
2024-03-18 13:53:10 -0400

    add ts

--- src/routes/products/[productId]/+page.svelte
+++ src/routes/products/[productId]/+page.svelte
@@ -1,7 +1,7 @@
-<script>
+<script lang="ts">
   import { page } from "$app/stores";
 
-  const productId = $page.params.productId;
+  const productId: string = $page.params.productId;
 </script>
 
 <h1>Details about product with ID: {productId}</h1>
