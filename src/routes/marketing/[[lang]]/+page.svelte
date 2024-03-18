23b2eae4deb6c2a6d6409ea346e08b94619d4687
leobewater <mmbee888@gmail.com>
2024-03-18 14:15:06 -0400

    optional route parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const greetings = {
+    en: "Hello!",
+    es: "Holda!",
+    fr: "Bonjour!",
+  };
+
+  // Assign default lang 
+  const { lang = 'en' } = $page.params;
+  const greeting: string = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
