f67b7a8a316916b162f31f548d091c77f28b057e
leobewater <mmbee888@gmail.com>
2024-03-18 21:07:15 -0400

    add post endpoint

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -9,3 +9,16 @@ export function GET() {
   // });
   return json(comments);
 }
+
+export async function POST(requestEvent) {
+  const { request } = requestEvent;
+  const { text } = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text,
+  };
+  comments.push(newComment);
+
+  // return new Response(JSON.stringify(newComment), { status: 201 });
+  return json(newComment, { status: 201 });
+}
